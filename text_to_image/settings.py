728ed180b3aa73ee2b6212e7b51ed568ad92d3c6
Amine Ghadi <72098445+Ghadi10A@users.noreply.github.com>
2024-05-18 14:53:29 +0100

    Update settings.py

--- text_to_image/settings.py
+++ text_to_image/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,11 +75,16 @@ WSGI_APPLICATION = "text_to_image.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        default='postgres://koyeb-adm:rgEscdf0o1lS@ep-yellow-term-a1trx1ch.ap-southeast-1.pg.koyeb.app/koyebdb',
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
@@ -116,7 +122,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Simplified static file serving.
+# https://whitenoise.evans.io/en/stable/django.html#django-middleware
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
